82108b4a9ce528bc534e37bc147aa9b17ed245b4
Tanya Kozyreva <tatiana.kozyreva1987@gmail.com>
2023-11-28 14:33:10 -0800

    modified:   cycles in JS/assign-cicles.js

--- cycles in JS/assign-cicles.js
+++ cycles in JS/assign-cicles.js
@@ -1,6 +1,41 @@
-for(let x in fruits){
-    console.log(fruits[x]);
-    if(fruits[x]=="avocado"){
-        console.log("I did not know that avocado is a fruit!!!");
+
+
+const emp1 = {
+    name: "John",
+    last_name: "Wick",
+    age: "35",
+    companies: ["Microsoft", "Google", "Amazon", "IPAM"]
+
+}
+const emp2 = {
+    name: "David",
+    last_name: "Bowie",
+    age: "43",
+    companies: ["Microsoft", "Meta", "Sony", "Sega", "Nintendo" ]
+
+
+    
+}
+
+
+    if (emp1.companies.length > emp2.companies.length) {
+        console.log (emp1.name)
+        
     }
+    else if (emp2.companies.length > emp1.companies.length) {
+        console.log (emp2.name)
+    } 
+    else {
+        console.log(emp1.name, ",", emp2.name );
     }
+
+
+// for (let index = 0; index < emp1.companies.length; index++) {
+//     console.log(emp1.companies[index]);
+//     console.log(emp2.companies[index]);
+// }
+
+
+
+
+
