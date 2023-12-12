22d4c03e7d33ac39b2e1430fc1685844fc8b893b
Beny Muler <crow7m@gmail.com>
2023-12-12 11:24:22 -0500

    Update expressions.js

--- module7/expressions.js
+++ module7/expressions.js
@@ -1,8 +1,19 @@
+//let year, birthYear;
+year = 2013;
+birthYear = 2000;
+
+if (year - birthYear >= 18) {
+    console.log('You are an adult');
+} else if (year - birthYear >= 13 && year - birthYear < 18) {
+    console.log('You are a teenager');
+} else if (year - birthYear < 13) {
+    console.log('You are a child');
+}
 
 let year, birthYear
 year = 2013
 birthYear = 2000
-
+ // the [] is wrong, and not needed here , please take a look at the code above, it is just regular math operations
 if ([year - birthYear] >= 18) {
     console.log('You are an adult'); 
 }
