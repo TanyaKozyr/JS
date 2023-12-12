64790eb0fb5aebfeeaf5e3db3947b7bf2be9bce5
Beny Muler <crow7m@gmail.com>
2023-12-12 11:18:04 -0500

    Update string&liter.js

--- module7/string&liter.js
+++ module7/string&liter.js
@@ -1,10 +1,16 @@
+
+// PLease do not use & in file names, file names best practices are stringLiteral.js or string_literal.js
+//same here, better approach will be function 
+
 let firstName, lastName, age;
 firstName = "Tatiana";
 lastName = "Kozyreva";
 birthYear = 1987
 year = 2023
-
-tatiana = `My name is ${firstName} ${lastName} and I'm ${year - birthYear} years old`;
-
-
-console.log(tatiana);
+const prinPersonInfo = ({name,lastName, birthYear, year })=>{
+const stringToPrint  = `My name is ${firstName} ${lastName} and I'm ${year - birthYear} years old`;
+ return stringToPrint
+}
+const result = prinPersonInfo({name: firstName, lastName:lastName,birthYear:birthYear, year:year });
+console.log(result);
+// or console.log(prinPersonInfo({name: firstName, lastName:lastName,birthYear:birthYear, year:year }))
