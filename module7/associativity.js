2dac5f88ea8bd1aadfafc20350de1a49f3ee1371
Beny Muler <crow7m@gmail.com>
2023-12-12 11:33:25 -0500

    Update associativity.js

--- module7/associativity.js
+++ module7/associativity.js
@@ -6,7 +6,9 @@ I have been living in America for 6 years and
 dreaming of becoming a developer. I have a family - 
 my husband, son and cat.`
 
-// let firstSentence = text.split('.')
+// let firstSentence = text.split('.') // Why this one is commented out ???
+// can also be done this way 
+let firstSentence = text.split('.')?.[0]; // here we know that array will be returned, so we grab the first element from that array after split()
 // console.log(firstSentence[0])
 
 let target = "I"
@@ -17,3 +19,7 @@ while ((pos = text.indexOf(target, pos + 1)) !== -1) {
  count++ ;
 }
 console.log(`Word "I" appears ${count} times in this text`)
+
+
+// the idea is great, same you can use function 
+//just put the while in there, and rturn count
