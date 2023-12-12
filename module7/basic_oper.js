91a2099437568e24dfbe52f9b0f76fa2debc4efb
Beny Muler <crow7m@gmail.com>
2023-12-12 11:12:28 -0500

    Update basic_oper.js

--- module7/basic_oper.js
+++ module7/basic_oper.js
@@ -5,7 +5,7 @@ area = (width*height)
 
 console.log ("Area of rectangle is", area,"cm")
 
-
+//while your code will work, usage of funtion is a better approach, cause it can be also be re-used, so the code below is better
 // function area(width, height) {
 //     return width*height
 // }
