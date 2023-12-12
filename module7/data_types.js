c878281b23be040f721f8ef8ea70078aea91fe62
Beny Muler <crow7m@gmail.com>
2023-12-12 11:10:15 -0500

    Update data_types.js
    PLease take a look at this way of solution

--- module7/data_types.js
+++ module7/data_types.js
@@ -1,15 +1,17 @@
 
-let studentName, age
+let studentName, age, isStudent // this way you need to declare the isStudent variable, however in this case 
+// const makes more sense. cause values are not being updated
+// so you can do it this way 
+const studentName = "Tanya Kozyreva";
+const age = '35';
+const isStudent = true;
 
-studentName = "Tanya Kozyreva";
-age = "35"
 
-if (studentName === "Tanya Kozyreva" && age === "35"){
-console.log(studentName, "is Student");
+//this way you can define a function, that will print, Tanya is student true, if isStudent is true, or will print ,  Tanya is student false if not
+const checkIsStudent = (someParamIsStudent, studentName)=>{
+        console.log(studentName, `"is Student" ${someParamIsStudent}`);
 }
-
-else {
-    console.log(studentName, "is not Student");
-}
+//call he function
+checkIsStudent(isStudent, studentName);
 
 // * I couldn’t apply the path with “true” and “false” because it’s not clear how to use them with these values
